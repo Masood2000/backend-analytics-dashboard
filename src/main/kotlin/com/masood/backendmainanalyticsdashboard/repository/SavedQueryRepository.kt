ad79ef10247c3faab9455728d573c0864d86e74f
Masood2000 <masudurrehman2000@gmail.com>
2025-11-18 14:51:25 +0100

    in reviews

--- src/main/kotlin/com/masood/backendmainanalyticsdashboard/repository/SavedQueryRepository.kt
+++ src/main/kotlin/com/masood/backendmainanalyticsdashboard/repository/SavedQueryRepository.kt
@@ -4,3 +4,5 @@ import com.masood.backendmainanalyticsdashboard.model.SavedQuery
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface SavedQueryRepository : JpaRepository<SavedQuery, Long>
+
+
