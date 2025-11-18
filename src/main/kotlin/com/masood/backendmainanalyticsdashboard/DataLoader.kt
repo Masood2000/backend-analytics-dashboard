b0fa0363842ab27cde022f034d79f2fe53bcae45
Masood2000 <masudurrehman2000@gmail.com>
2025-11-18 14:51:47 +0100

    in reviews

--- src/main/kotlin/com/masood/backendmainanalyticsdashboard/DataLoader.kt
+++ src/main/kotlin/com/masood/backendmainanalyticsdashboard/DataLoader.kt
@@ -7,10 +7,6 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 import java.io.InputStreamReader
 
-
-
-
-
 /***
  * Data loader class to initially fill the h2 database with the
  * csv file contents...
