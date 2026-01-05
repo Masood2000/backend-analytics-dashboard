269e52de93a4be8259ae2edf91560abd69fb05be
Masood2000 <masudurrehman2000@gmail.com>
2026-01-05 20:17:39 +0100

    in reviews..

--- src/main/kotlin/com/masood/backendmainanalyticsdashboard/BackendMainAnalyticsDashboardApplication.kt
+++ src/main/kotlin/com/masood/backendmainanalyticsdashboard/BackendMainAnalyticsDashboardApplication.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class BackendMainAnalyticsDashboardApplication
 
+
 fun main(args: Array<String>) {
     runApplication<BackendMainAnalyticsDashboardApplication>(*args)
 }
